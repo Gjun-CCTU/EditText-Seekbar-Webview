16e9f76c378f4adf1b94731efc4660fd3c9014e8
pighaddt <pighaddt@gmail.com>
2019-11-16 12:02:17 +0800

    Initial commit

---
+++ app/src/main/java/com/angus/gjun_android_edittext/MainActivity.java
@@ -0,0 +1,51 @@
+package com.angus.gjun_android_edittext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1, btn2;
+    EditText et1, et2;
+    TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        et1 = (EditText) findViewById(R.id.et1);
+        et2 = (EditText) findViewById(R.id.et2);
+        tv1 = (TextView) findViewById(R.id.tv1);
+
+        View.OnClickListener OCL = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int id = v.getId();
+                switch (id){
+                    case R.id.btn1:
+                        double h  = Double.parseDouble(et1.getText().toString());
+                        double w  = Double.parseDouble(et2.getText().toString());
+                        double bmi = w/(h/100 * h/100);
+                        tv1.setText("身高 : " + h + "cm \n 體重 : " + w + "kg \n bmi :" + bmi);
+                        break;
+
+                    case R.id.btn2:
+                        et1.setText("");
+                        et2.setText("");
+                        tv1.setText("");
+                        break;
+                }
+            }
+        };
+
+        btn1.setOnClickListener(OCL);
+        btn2.setOnClickListener(OCL);
+
+    }
+}
